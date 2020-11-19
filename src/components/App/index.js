d52bd80898853915f917a22e8262153ed4a69df9
Axel-Baudain <axel.baudain@gmail.com>
2020-11-19 15:15:20 +0100

    Added some CSS rules and illustration

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -4,10 +4,19 @@ import './style.css';
 import Header from '../Header';
 import Footer from '../Footer';
 
+const image = 'https://images.unsplash.com/photo-1605142859862-978be7eba909?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80';
+
 const App = () => (
-  <div className="App">
+  <div className="app">
     <Header />
-    Content
+    <div className="content__container">
+      <div className="content__header" style={{ backgroundImage: `url('${image}')`, backgroundRepeat: `no-repeat`, backgroundSize: `cover`, backgroundPosition: `center` }}>
+        <div className="header__text">
+          <h1 className="header__title">Scenarioz</h1>
+          <p className="header__subtitle">La bibliothèque de scénarios faite par un MJ, pour des MJs.</p>
+        </div>
+      </div>
+    </div>
     <Footer />
   </div>
 );
--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import './style.css';
 
 const Footer = () => (
-  <div>
+  <div className="footer">
     footer
   </div>
 );
