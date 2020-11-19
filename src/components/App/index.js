55ebbe4f0718deb669ee74d02c0691f2f56de510
Axel-Baudain <axel.baudain@gmail.com>
2020-11-19 14:38:16 +0100

    Base structure has been set

---
+++ src/components/App/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './style.css';
+
+import Header from '../Header';
+import Footer from '../Footer';
+
+const App = () => (
+  <div className="App">
+    <Header />
+    Content
+    <Footer />
+  </div>
+);
+
+export default App;
---
+++ src/components/Footer/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './style.css';
+
+const Footer = () => (
+  <div>
+    footer
+  </div>
+);
+
+export default Footer;
