9868863e7121bf0c646ce8523538dee9bd1a20ef
Axel-Baudain <axel.baudain@gmail.com>
2020-11-20 10:48:01 +0100

    Hover behavior on links is done

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -5,10 +5,10 @@ import './style.css';
 
 const Header = () => (
   <div className="header">
-    <NavLink className="header__navigation--element" to="/">Scenarioz</NavLink>
+    <NavLink className="header__navigation--element" exact to="/">Scenarioz</NavLink>
     <div className="header__navigation">
-      <NavLink className="header__navigation--element" to="/scenarios">Scénarios</NavLink>
-      <NavLink className="header__navigation--element" to="/creators">Créateurs</NavLink>
+      <NavLink className="header__navigation--element" exact to="/scenarios">Scénarios</NavLink>
+      <NavLink className="header__navigation--element" exact to="/creators">Créateurs</NavLink>
     </div>
     <i class="fas fa-bars" />
   </div>
