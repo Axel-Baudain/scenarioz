b5613d71ba9c952e7fe4fd1a33826f1fd90bb41a
Axel-Baudain <axel.baudain@gmail.com>
2020-11-24 18:06:28 +0100

    WIP - Side menu

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,35 +1,96 @@
 import React from 'react';
 import { Route, Switch, Link } from 'react-router-dom';
+import {
+Checkbox,
+Grid,
+Header,
+Icon,
+Image,
+Menu,
+Segment,
+Sidebar,
+} from 'semantic-ui-react';
+
 import './style.css';
 
-import Header from '../Header';
+import WebHeader from '../Header';
 import Footer from '../Footer';
 import Scenarios from '../Scenarios';
 import Creators from '../Creators';
 
 const image = 'https://images.unsplash.com/photo-1605142859862-978be7eba909?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80';
 
+const SidebarExampleSidebar = () => {
+  const [visible, setVisible] = React.useState(false)
+
+  return (
+    <Grid columns={1}>
+      <Grid.Column>
+        <Checkbox
+          checked={visible}
+          label={{ children: <code>visible</code> }}
+          onChange={(e, data) => setVisible(data.checked)}
+        />
+      </Grid.Column>
+
+      <Grid.Column>
+        <Sidebar.Pushable as={Segment}>
+          <Sidebar
+            as={Menu}
+            animation='overlay'
+            icon='labeled'
+            inverted
+            onHide={() => setVisible(false)}
+            vertical
+            visible={visible}
+            width='thin'
+          >
+            <Menu.Item as='a'>
+              <Icon name='home' />
+              Home
+            </Menu.Item>
+            <Menu.Item as='a'>
+              <Icon name='gamepad' />
+              Games
+            </Menu.Item>
+            <Menu.Item as='a'>
+              <Icon name='camera' />
+              Channels
+            </Menu.Item>
+          </Sidebar>
+
+          <Sidebar.Pusher>
+            <Segment basic>
+              <Header as='h3'>Application Content</Header>
+              <Image src='/images/wireframe/paragraph.png' />
+            </Segment>
+          </Sidebar.Pusher>
+        </Sidebar.Pushable>
+      </Grid.Column>
+    </Grid>
+  )
+}
+
 const App = () => (
   <div className="app">
-    <Header />
+    <WebHeader />
+    <SidebarExampleSidebar />
     <Switch>
-    <Route exact path="/"> 
-    <div className="content__container">
-      <div className="content__header" style={{ backgroundImage: `url('${image}')`, backgroundRepeat: `no-repeat`, backgroundSize: `cover`, backgroundPosition: `center` }}>
-        <div className="header__text">
-          <h1 className="header__title">Scenarioz</h1>
-          <p className="header__subtitle">La bibliothèque de scénarios, faite par un Maître de Jeux, pour les Maîtres de Jeux.</p>
-          <Link ><button>Trouver le scénario parfait</button></Link>
+      <Route exact path="/">
+        <div fluid className="content__home">
+          <div className="home__text">
+            <h1 className="home__title">Scenarioz</h1>
+            <p className="home__subtitle">La bibliothèque de scénarios, faite par un Maître de Jeux, pour les Maîtres de Jeux.</p>
+            <Link to="/scenarios" className="home__button">Trouver le scénario parfait</Link>
+          </div>
         </div>
-      </div>
-    </div>
-    </Route>
-    <Route exact path="/scenarios">
-      <Scenarios />
-    </Route>
-    <Route exact path="/creators">
-      <Creators />
-    </Route>
+      </Route>
+      <Route exact path="/scenarios">
+        <Scenarios />
+      </Route>
+      <Route exact path="/creators">
+        <Creators />
+      </Route>
     </Switch>
     <Footer />
   </div>
